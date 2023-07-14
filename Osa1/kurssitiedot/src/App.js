bfec77de7bae9a2f3ba21bf923068d7e54cd7681
Janne <janne.primietta@gmail.com>
2023-07-14 16:47:52 +0300

    1.3

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -36,7 +36,6 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  console.log(props);
   return (
     <div>
       <Part part={props.part1} exercises={props.exercises1} />
@@ -65,7 +64,33 @@ const Total = (props) => {
     </div>
   );
 };
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1.name} exercises1={part1.exercises} />
+      <Content part2={part2.name} exercises2={part2.exercises} />
+      <Content part3={part3.name} exercises3={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  );
+};
+
+/*
 const App = () => {
   // const-määrittelyt
   const course = "Half Stack application development";
@@ -85,4 +110,5 @@ const App = () => {
     </div>
   );
 };
+*/
 export default App;
